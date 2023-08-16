e5b3c2d38515ccbcd38227b433065432ec3b4e29
ChandraShekar2001 <pvchandu07@gmail.com>
2023-08-16 19:41:00 +0530

    final commit

--- question2/controllers/numberController.js
+++ question2/controllers/numberController.js
@@ -2,24 +2,33 @@ const axios = require("axios");
 
 exports.numbers = async (req, res) => {
   try {
-    const regexPattern = /^(http|https):\/\/\d+\.\d+\.\d+\.\d+\/\w+\/\w+$/;
+    const regexPattern = /^http:\/\/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/numbers\/\w+$/;
 
     const query  = req.query;
     const {url} = query;
-    url.push("www")
-    console.log(url);
+
     const filteredUrl = [];
     for(let i = 0; i < url.length; i++){
         if(regexPattern.test(url[i])){
             filteredUrl.push(url[i]);
         }
     }
-
-
+    let raw = [];
+    for(let i = 0; i < filteredUrl.length; i++){
+        let {data} = await axios.get(filteredUrl[i]);
+        console.log(data.numbers);  
+        raw = [...raw, ...data.numbers];
+    }
+    raw = [...new Set(raw)];
+    raw.sort((a, b) => a - b);
     
     res.status(200).json({
-        status: "success",
-        filteredUrl
+        numbers: raw,
     });
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({
+        status: "fail",
+        message: error.message,
+    });
+  }
 };
